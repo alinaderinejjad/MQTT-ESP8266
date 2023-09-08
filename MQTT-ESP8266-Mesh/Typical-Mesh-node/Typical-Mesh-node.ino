03ed894bb8b13e68277db6309833e7fb5e890512
Ali Naderinezhad <ali.naderinejjad@gmail.com>
2023-09-09 02:16:25 +0330

    the code from last edit was in a wrong directory.corrected it

--- MQTT-ESP8266-Mesh/Typical-Mesh-node/Typical-Mesh-node.ino
+++ MQTT-ESP8266-Mesh/Typical-Mesh-node/Typical-Mesh-node.ino
@@ -1,69 +1,77 @@
+#include <painlessMesh.h>
 
-#include "painlessMesh.h"
+#define   STATION_SSID     "Red Phoenix"
+#define   STATION_PASSWORD "PhoenixPhoenix"
 
-// AP Const defines
-#define   MESH_PREFIX     "Mesh-Eval"
-#define   MESH_PASSWORD   "mqtt-mesh"
-// Mesh Const defines
-#define   MESH_PORT       2193
 
-Scheduler userScheduler; // to control your personal task
-painlessMesh  mesh;
+#define   MESH_PREFIX     "enigmamachine"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+#define   mesh_channel  6
 
-// Functions Prototype
-void sendMessage() ; 
+
+#define HOSTNAME "MQTT_Handler"
+
+
+Scheduler myScheduler; 
+painlessMesh  localmesh;
+WiFiClient wificli;
+
+
+void sendMessage() ;
 
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
-void sendMessage() {
+void sendMessage()
+{
   String msg = "Hello from node ";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
+  msg += localmesh.getNodeId();
+  localmesh.sendBroadcast( msg );
   taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
 }
 
-// Recieve callback Routin
-void receivedCallback( uint32_t from, String &msg ) {
+
+void receivedCallback( uint32_t from, String &msg ) 
+{
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
 
-// new connection callback Routin
-void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
 
-// change connection callback Routin
-void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
-}
 
-// node time adjustion callback Routin-needed for nodes synchronization in mesh protocol
-void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-}
+
+
 
 void setup() {
+
   Serial.begin(115200);
-  Serial.printf("attempting to connect to %s", MESH_PREFIX );
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  Serial.printf("started");
 
-  userScheduler.addTask( taskSendMessage );
+    localmesh.init( MESH_PREFIX, MESH_PASSWORD,&myScheduler, MESH_PORT, WIFI_AP_STA, mesh_channel );
+    localmesh.onReceive(&receivedCallback);
+    localmesh.stationManual(STATION_SSID, STATION_PASSWORD);
+    
+    //uncomment bellow line for the bridge node 
+    //localmesh.setHostname(HOSTNAME);
+
+
+    //uncomment bellow line for the bridge node
+    //localmesh.setRoot(true);
+    
+    localmesh.setContainsRoot(true);
+
+
+  myScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
+
 }
 
 
 
-//
-// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
-// 2. prints anything it receives to Serial.print
-//
-//
-
-void loop() {
-  //user scheduller is called inside mesh.update
-  mesh.update();
+void loop() 
+{
+localmesh.update();
 }
+
+
+
+
